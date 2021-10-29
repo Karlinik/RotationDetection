d569230062a53f4a2e80d91b25f60679e40bc9b4
Nikola <karlikova.nikola@gmail.com>
2021-10-29 19:37:41 +0200

    resnet pretrained weights added

---
+++ libs/configs/OPENPNP/retinanet/cfgs_res50_openpnp_v6.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from __future__ import division, print_function, absolute_import
+
+import numpy as np
+
+from libs.configs._base_.models.retinanet_r50_fpn import *
+from libs.configs._base_.datasets.openpnp_detection import *
+from libs.configs._base_.schedules.schedule_1x import *
+from dataloader.pretrained_weights.pretrain_zoo import PretrainModelZoo
+
+# schedule
+BATCH_SIZE = 1
+GPU_GROUP = "0,1,2"
+NUM_GPU = len(GPU_GROUP.strip().split(','))
+SAVE_WEIGHTS_INTE = 20673 * 2
+DECAY_STEP = np.array(DECAY_EPOCH, np.int32) * SAVE_WEIGHTS_INTE
+MAX_ITERATION = SAVE_WEIGHTS_INTE * MAX_EPOCH
+WARM_SETP = int(WARM_EPOCH * SAVE_WEIGHTS_INTE)
+
+# dataset
+DATASET_NAME = 'OPENPNPTrain'
+
+# model
+# backbone
+pretrain_zoo = PretrainModelZoo()
+PRETRAINED_CKPT = pretrain_zoo.pretrain_weight_path(NET_NAME, ROOT_PATH)
+TRAINED_CKPT = os.path.join(ROOT_PATH, 'output/trained_weights')
+
+# loss
+CLS_WEIGHT = 1.0
+REG_WEIGHT = 1.0 / 5.0
+REG_LOSS_MODE = 1  # IoU-Smooth L1
+
+VERSION = 'RetinaNet_OPENPNP_1x_20210125'
---
+++ libs/configs/OPENPNP/retinanet/cfgs_res50_openpnp_v8.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from __future__ import division, print_function, absolute_import
+
+import numpy as np
+
+from libs.configs._base_.models.retinanet_r50_fpn import *
+from libs.configs._base_.datasets.openpnp_detection import *
+from libs.configs._base_.schedules.schedule_1x import *
+from dataloader.pretrained_weights.pretrain_zoo import PretrainModelZoo
+
+# schedule
+BATCH_SIZE = 1
+GPU_GROUP = "0"
+NUM_GPU = len(GPU_GROUP.strip().split(','))
+SAVE_WEIGHTS_INTE = 20673
+DECAY_STEP = np.array(DECAY_EPOCH, np.int32) * SAVE_WEIGHTS_INTE
+MAX_ITERATION = SAVE_WEIGHTS_INTE * MAX_EPOCH
+WARM_SETP = int(WARM_EPOCH * SAVE_WEIGHTS_INTE)
+
+# dataset
+DATASET_NAME = 'OPENPNPTrain'
+
+# model
+# backbone
+pretrain_zoo = PretrainModelZoo()
+PRETRAINED_CKPT = pretrain_zoo.pretrain_weight_path(NET_NAME, ROOT_PATH)
+TRAINED_CKPT = os.path.join(ROOT_PATH, 'output/trained_weights')
+
+# bbox head
+METHOD = 'R'
+ANCHOR_RATIOS = [1, 1 / 3., 3.]
+
+# loss
+CLS_WEIGHT = 1.0
+REG_WEIGHT = 1.0 / 5.0
+REG_LOSS_MODE = None
+
+VERSION = 'RetinaNet_OPENPNP_1x_20210617'
